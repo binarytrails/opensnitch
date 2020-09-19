a91ee2b132d6561c4ecccbe163397372656c8edb
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-09-19 13:27:00 +0200

    cli: display stats by type of all nodes

--- server/cli/views/bytype.go
+++ server/cli/views/bytype.go
@@ -3,11 +3,14 @@ package views
 import (
 	"sort"
 	"time"
+
+	"github.com/gustavo-iniguez-goya/opensnitch/daemon/ui/protocol"
+	"github.com/gustavo-iniguez-goya/opensnitch/server/api/nodes"
 )
 
 const (
-	// hits column is 5chars width
-	colHits    = "Hits "
+	// hits column is 10chars wide
+	colHits    = "Hits    "
 	colHost    = "Host"
 	colProcess = "Process"
 	colAddress = "Address"
@@ -15,15 +18,15 @@ const (
 	colUID     = "UID"
 )
 
-type event struct {
+type eventHits struct {
 	Key   string
 	Value uint64
 }
 
-func sortEvents(events map[string]uint64) *[]*event {
-	var eves []*event
-	for k, v := range events {
-		eves = append(eves, &event{k, v})
+func sortEvents(events *map[string]uint64) *[]*eventHits {
+	var eves []*eventHits
+	for k, v := range *events {
+		eves = append(eves, &eventHits{k, v})
 	}
 	sort.Slice(eves, func(i, j int) bool {
 		if sortMode == sortModeAscending {
@@ -34,43 +37,73 @@ func sortEvents(events map[string]uint64) *[]*event {
 	return &eves
 }
 
+func getTypeStats(viewType string, stype *protocol.Statistics) (vstats *map[string]uint64) {
+	vstats = &stype.ByHost
+	switch viewType {
+	case ViewProcs:
+		vstats = &stype.ByExecutable
+	case ViewAddrs:
+		vstats = &stype.ByAddress
+	case ViewPorts:
+		vstats = &stype.ByPort
+	case ViewUsers:
+		vstats = &stype.ByUid
+	}
+
+	return vstats
+}
+
+func getColumnName(viewType string) (colName string) {
+	switch viewType {
+	case ViewProcs:
+		colName = colProcess
+	case ViewAddrs:
+		colName = colAddress
+	case ViewPorts:
+		colName = colPort
+	case ViewUsers:
+		colName = colUID
+	default:
+		colName = colHost
+	}
+
+	return colName
+}
+
 // StatsByType shows the latest statistics of the node(s) by type.
-func StatsByType(vtype string) {
+func StatsByType(viewType string) {
 	waitForStats()
-	colWhat := colHost
-	topCols := []string{colHits, "-", colWhat}
+	colWhat := getColumnName(viewType)
+	topCols := []string{"Node                  ", colHits, "-", colWhat}
+	totalStats := 0
 
 	for {
 		if !getPauseStats() {
-			vstats := config.apiClient.GetLastStats().ByHost
-			switch vtype {
-			case ViewProcs:
-				vstats = config.apiClient.GetLastStats().ByExecutable
-				colWhat = colProcess
-			case ViewAddrs:
-				vstats = config.apiClient.GetLastStats().ByAddress
-				colWhat = colAddress
-			case ViewPorts:
-				vstats = config.apiClient.GetLastStats().ByPort
-				colWhat = colPort
-			case ViewUsers:
-				vstats = config.apiClient.GetLastStats().ByUid
-				colWhat = colUID
-			}
 			resetScreen()
-			topCols[2] = colWhat
+			topCols[3] = colWhat
 			showTopBar(topCols)
 
-			for _, e := range *sortEvents(vstats) {
-				if config.Filter != "" {
-					if e.Key == config.Filter {
-						printStats(e.Key, e.Value)
-					}
-				} else {
-					printStats(e.Key, e.Value)
+			totalStats = 0
+			for addr, node := range *nodes.GetAll() {
+				if node.GetStats() == nil {
+					break
 				}
+
+				vstats := getTypeStats(viewType, node.GetStats())
+				for _, e := range *sortEvents(vstats) {
+					// XXX: actions should be modular
+					if config.Filter != "" {
+						if e.Key == config.Filter {
+							printStats(addr, e.Key, e.Value)
+						}
+					} else {
+						printStats(addr, e.Key, e.Value)
+					}
+				}
+
+				totalStats += len(*vstats)
 			}
-			printVerticalPadding(len(vstats))
+			printVerticalPadding(totalStats)
 		}
 		showStatusBar()
 		readLiveMenu()
