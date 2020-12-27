e34fb01643826dbdaca09e5ab59c8e2a2fb2f63e
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-12-27 11:34:12 +0100

    improved listening on unix sockets
    - Avoid listening for nodes if there's another UI running.
    - Exit if we can't delete a residual unix socket file.

--- server/api/api.go
+++ server/api/api.go
@@ -16,9 +16,11 @@ it must follow these steps:
 
 import (
 	"net"
+	"os"
 	"time"
 
 	"github.com/evilsocket/opensnitch/daemon/ui/protocol"
+	"github.com/gustavo-iniguez-goya/opensnitch/daemon/core"
 	"github.com/gustavo-iniguez-goya/opensnitch/daemon/log"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -64,8 +66,33 @@ func (s *server) Notifications(streamChannel protocol.UI_NotificationsServer) er
 	return nil
 }
 
+// socketIsReady determines if we can listen for new nodes
+func socketIsReady(proto, port string) bool {
+	if proto != "unix" {
+		return true
+	}
+	// connect to the unix socket:
+	// - if we can connect, there's another UI listening for nodes, so we can't continue.
+	// - if not, remove the unix socket file to avoid "address already in use" error.
+	_, err := net.Dial(proto, port)
+	if err == nil {
+		log.Fatal("There's another GUI/TUI/*UI running. Please, close it before launching this UI.")
+		return false
+	}
+	if core.Exists(port) {
+		if err := os.Remove(port); err != nil {
+			log.Fatal("Can not listen for new clients. Try again removing %s first.", port)
+		}
+	}
+	return true
+}
+
 // StartServer start listening for incoming nodes/clients.
 func startServer(client *Client, proto, port string) {
+	if socketIsReady(proto, port) == false {
+		return
+	}
+
 	sockFd, err := net.Listen(proto, port)
 	if err != nil {
 		log.Error("failed to listen on %s: %v", port, err)
